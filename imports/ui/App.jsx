380e83715bde5d279c3eca6eb99e40b4c9948ea9
waddah-jd <waddah.ja@gmail.com>
2021-03-29 07:02:27 +0300

    add AUTH constants

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -4,18 +4,19 @@ import { useTracker } from "meteor/react-meteor-data";
 import PrivateComponent from "./PrivateComponent";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
+import AUTH_CONSTANTS from "../../constants/auth";
 
 export default () => {
   const user = useTracker(() => Meteor.user());
-  const [shownPage, setShownPage] = useState("SIGN_IN");
+  const [shownPage, setShownPage] = useState(AUTH_CONSTANTS.SIGN_IN);
 
-  const showSignIn = () => setShownPage("SIGN_IN");
-  const showSignUp = () => setShownPage("SIGN_UP");
+  const showSignIn = () => setShownPage(AUTH_CONSTANTS.SIGN_IN);
+  const showSignUp = () => setShownPage(AUTH_CONSTANTS.SIGN_UP);
 
   if (!user) {
     return (
       <div>
-        {shownPage === "SIGN_IN" ? (
+        {shownPage === AUTH_CONSTANTS.SIGN_IN ? (
           <SignIn showSignUp={showSignUp} />
         ) : (
           <SignUp showSignIn={showSignIn} />
